75c3b7ce2ed92e2e0dda459d2670a57723e8ad14
imnotus <39824578+imnotus@users.noreply.github.com>
2023-07-26 17:09:04 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ const double POWER = 1.0;
 //出力ファイルを開く
 ofstream outputfile1;
 ofstream outputfile2;
+ofstream outputfile3;
+ofstream outputfile4;
 
 struct pos {
     double x;
@@ -27,7 +29,7 @@ struct terminal {
     //double to_BS[num_BS];
     double dst_to_origin;
     int nearest_BS;
-    //double gain;
+    double level;
     double coef;
     bool state; //True: in , False: out
     bool operator<( const terminal& right ) const {
@@ -137,7 +139,7 @@ void SIC_NOMA_thp(double lambda_IoT, double alpha, double noise) {
     }
     ifs.close();
     unsigned long num_BS = BS_pos.size();
-//    int num_BS = poisson_dist(4 * radius * radius * lambda_BS);
+//    int num_BS = poisson_dist(PI * radius * radius * lambda_BS);
 //    //BSの座標を設定
 //    vector<pair<double, double>> BS_pos(num_BS);
     pair<double, double> origin = make_pair(0, 0);
@@ -151,7 +153,7 @@ void SIC_NOMA_thp(double lambda_IoT, double alpha, double noise) {
     bool p_flag[4] = {true, true, true, true};
     cout << "Progress is 0%";
     for (int t = 0; t < end_time; t++) {
-        int num_IoT = poisson_dist(4 * radius * radius * lambda_IoT);
+        int num_IoT = poisson_dist(PI * radius * radius * lambda_IoT);
         
         double SI = 0;
         vector<terminal> device(num_IoT);
@@ -162,10 +164,10 @@ void SIC_NOMA_thp(double lambda_IoT, double alpha, double noise) {
             device.at(i).pos = coordinate();
             double dst_to = cal_dst(device.at(i).pos, origin);
             device.at(i).dst_to_origin = dst_to;
-            if (dst_to < 30) {
+            if (dst_to < 30) { //原点基地局にアクセスする端末を探す
                 for (int j = 0; j < num_BS; j++) {
                     double dst = cal_dst(device.at(i).pos, BS_pos.at(j));
-                    device.at(i).state = true;
+                    device.at(i).state = true; //送信先が原点ならtrue
                     if (dst < device.at(i).dst_to_origin) {
                         device.at(i).state = false;
                         break;
@@ -228,19 +230,255 @@ void SIC_NOMA_thp(double lambda_IoT, double alpha, double noise) {
     //outputfile << res << " " << I << " ";
 }
 
+
+//Power allocation
+void PA_NOMA_pos(double lambda_IoT, double alpha, double noise, double L) {
+    double success = 0.0;
+    cout << "pass loss exp : " << alpha << endl;
+    
+    //座標読み込み
+    string filename1 = "BS_pos.txt";
+    vector<pair<double, double>> BS_pos;
+    ifstream readingfile;
+    readingfile.open(filename1);
+    ifstream ifs("BS_pos.txt");
+        if (ifs.fail()) {
+           cerr << "Cannot open file\n";
+           exit(0);
+        }
+    double x, y;
+    while (ifs >> x >> y) {
+        BS_pos.push_back(make_pair(x, y));
+        //cout << x << " " << y << endl;
+    }
+    ifs.close();
+    unsigned long num_BS = BS_pos.size();
+//    int num_BS = poisson_dist(PI * radius * radius * lambda_BS);
+//    //BSの座標を設定
+//    vector<pair<double, double>> BS_pos(num_BS);
+    pair<double, double> origin = make_pair(0, 0);
+    BS_pos.at(0) = origin;
+//    for (int i = 1; i < num_BS; i++) {
+//        BS_pos.at(i) = coordinate();
+//        outputfile1 << BS_pos.at(i).first << " " << BS_pos.at(i).second << endl;
+//    }
+    
+    
+    bool p_flag[4] = {true, true, true, true};
+    cout << "Progress is 0%";
+    for (int t = 0; t < end_time; t++) {
+        int num_IoT = poisson_dist(PI * radius * radius * lambda_IoT);
+        
+        double SI = 0;
+        vector<double> LEV(L, 0);
+        vector<terminal> device(num_IoT);
+        vector<int> acl;
+        //端末情報を初期化
+        for (int i = 0; i < num_IoT; i++) {
+            //pair<double, double> pos = coordinate();
+            device.at(i).pos = coordinate();
+            double dst_to = cal_dst(device.at(i).pos, origin);
+            device.at(i).dst_to_origin = dst_to;
+            if (dst_to < 5) { //原点基地局にアクセスする端末を探す
+                for (int j = 0; j < num_BS; j++) {
+                    double dst = cal_dst(device.at(i).pos, BS_pos.at(j));
+                    device.at(i).state = true; //送信先が原点ならtrue
+                    if (dst < device.at(i).dst_to_origin) {
+                        device.at(i).state = false;
+                        break;
+                    }
+                    if (j == num_BS - 1) acl.push_back(i);
+                }
+            }
+            
+            if (device.at(i).state) { //原点にアクセスするならallocateされた電力を足す．
+                int level;
+                if (device.at(i).dst_to_origin > 1) level = L;
+                else level = L * device.at(i).dst_to_origin;
+                device.at(i).level = level;
+                device.at(i).coef = theta * pow(theta + 1, L - level);
+                LEV.at(level-1) += theta * pow(theta + 1, L - level);
+            } else {
+                //端末-基地局間のフェージング係数を設定
+                double H = exp_dist(1.0);//gauss_rand(0, 1);
+                double coef = H / pow(device.at(i).dst_to_origin, alpha);
+                device.at(i).coef = coef;
+                SI += coef;
+            }
+        }
+        
+        int s = (int)acl.size();
+        for (int i = 0; i < s; i++) {
+            int f = acl.at(i);
+            double P = device.at(f).coef;
+            int l = device.at(f).level;
+            double si = 0;
+            for (int j = 0; j <= l; j++) {
+                si += LEV.at(j);
+            }
+            double SINR = P / (SI + si - P + noise);
+            if (SINR > theta) {
+                success++;
+                outputfile4 << device.at(i).pos.first << " " << device.at(i).pos.second << endl;
+            }
+        }
+        
+        
+        //進捗状況を表示
+        double progress = t / end_time;
+        if (progress > 0.8 && p_flag[3]) {cout << "...80%"; p_flag[3] = false;}
+        else if (progress > 0.6 && p_flag[2]) {cout << "...60%"; p_flag[2] = false;}
+        else if (progress > 0.4 && p_flag[1]) {cout << "...40%"; p_flag[1] = false;}
+        else if (progress > 0.2 && p_flag[0]) {cout << "...20%"; p_flag[0] = false;}
+    }
+    cout << "...100%" << endl << endl;
+    
+    //double res = success / end_time;
+    //double I = thp_theo(lambda_IoT, lambda_BS, alpha);
+    //cout << "Throughput : " <<  res << " " << I << endl << endl;
+    //outputfile << res << " " << I << " ";
+}
+
+//Power allocated NOMA
+void PA_NOMA_thp(double lambda_IoT, double alpha, double noise, double L) {
+    double success = 0.0;
+    cout << "pass loss exp : " << alpha << endl;
+    
+    bool p_flag[4] = {true, true, true, true};
+    cout << "Progress is 0%";
+    for (int i = 0; i < end_time; i++) {
+        int num_IoT = poisson_dist(PI * radius * radius * lambda_IoT);
+        int num_BS = poisson_dist(PI * radius * radius * lambda_BS);
+        //BSの座標を設定
+        vector<pair<double, double>> BS_pos(num_BS);
+        pair<double, double> origin = make_pair(0, 0);
+        BS_pos.at(0) = origin;
+        for (int i = 1; i < num_BS; i++) {
+            BS_pos.at(i) = coordinate();
+        }
+        
+        double SI = 0;
+        vector<double> LEV(L, 0);
+        vector<terminal> device(num_IoT);
+        vector<int> acl;
+        //端末情報を初期化
+        for (int i = 0; i < num_IoT; i++) {
+            pair<double, double> pos = coordinate();
+            double dst_to = cal_dst(pos, origin);
+            device.at(i).dst_to_origin = dst_to;
+            if (dst_to < 5) { //原点の基地局にアクセスするデバイスを探す
+                for (int j = 0; j < num_BS; j++) {
+                    double dst = cal_dst(pos, BS_pos.at(j));
+                    device.at(i).state = true;
+                    if (dst < device.at(i).dst_to_origin) {
+                        device.at(i).state = false;
+                        break;
+                    }
+                    if (j == num_BS - 1) acl.push_back(i);
+                }
+            }
+            //チャネル条件の逆数をかけて消えるので今は無視
+            if (device.at(i).state) { //原点にアクセスするならallocateされた電力を足す．
+                int level;
+                if (device.at(i).dst_to_origin > 1) level = L;
+                else level = L * device.at(i).dst_to_origin;
+                device.at(i).level = level;
+                device.at(i).coef = theta * pow(theta + 1, L - level);
+                LEV.at(level-1) += theta * pow(theta + 1, L - level);
+            } else {
+                //端末-基地局間のフェージング係数を設定
+                double H = exp_dist(1.0);//gauss_rand(0, 1);
+                double coef = H / pow(device.at(i).dst_to_origin, alpha);
+                device.at(i).coef = coef;
+                SI += coef;
+            }
+            
+        }
+        
+        int s = (int)acl.size();
+        for (int i = 0; i < s; i++) {
+            int f = acl.at(i);
+            double P = device.at(f).coef;
+            int l = device.at(f).level;
+            double si = 0;
+            for (int j = 0; j <= l; j++) {
+                si += LEV.at(j);
+            }
+            double SINR = P / (SI + si - P + noise);
+            if (SINR > theta) success++;
+        }
+        
+        
+        //進捗状況を表示
+        double progress = i / end_time;
+        if (progress > 0.8 && p_flag[3]) {cout << "...80%"; p_flag[3] = false;}
+        else if (progress > 0.6 && p_flag[2]) {cout << "...60%"; p_flag[2] = false;}
+        else if (progress > 0.4 && p_flag[1]) {cout << "...40%"; p_flag[1] = false;}
+        else if (progress > 0.2 && p_flag[0]) {cout << "...20%"; p_flag[0] = false;}
+    }
+    cout << "...100%" << endl;
+    
+    double res = success / end_time;
+    //double I = thp_theo(lambda_IoT, lambda_BS, alpha);
+    cout << "Throughput : " <<  res << endl << endl;
+    outputfile3 << res << " ";
+}
+
+
+//void decode_dst() {
+//    //座標読み込み
+//    string filename1 = "2.0Second_decode_pos.txt";
+//    vector<pair<double, double>> BS_pos;
+//    ifstream readingfile;
+//    readingfile.open(filename1);
+//    ifstream ifs("2.0Second_decode_pos.txt");
+//        if (ifs.fail()) {
+//           cerr << "Cannot open file\n";
+//           exit(0);
+//        }
+//    ofstream outputfile3;
+//    string filename3 = "2.0_4.5_secondm.txt";
+//    outputfile3.open(filename3);
+//    double x, y;
+//    while (ifs >> x >> y) {
+//        if (x <= 2.0 && y <= 2.0) {
+//            outputfile3 << x << " " << y << endl;
+//        }
+//    }
+//    ifs.close();
+//
+//}
+
+
 int main() {
+    cout << "Pos 0, Thp 1 :";
+    double key; cin >> key; cout << endl;
+    cout << "Put start lambda IoT : ";
+    double k; cin >> k; cout << endl;
+    
     string filename2 = "Second_decode_pos.txt";
     string filename1 = "First_decode_pos.txt";
+    string filename3 = to_string(k) + "PA_NOMA_thp.txt";
+    string filename4 = to_string(k) + "PA_NOMA_pos.txt";
     //outputfile1.open(filename1);
     outputfile2.open(filename2);
     outputfile1.open(filename1);
+    outputfile3.open(filename3);
+    outputfile4.open(filename4);
     
-    cout << "Put start lambda IoT : ";
-    double k; cin >> k; cout << endl;
-    for (double alpha = 2.5; alpha <= 4.5; alpha += 0.5) {
-        SIC_NOMA_thp(k, alpha, 0);
+//    for (double alpha = 2.5; alpha <= 4.5; alpha += 0.5) {
+//        SIC_NOMA_thp(k, alpha, 0);
+//    }
+    outputfile3 << k << " ";
+    for (double L = 3.0; L <= 5; L++) {
+        double alpha = 4.5;
+        if (key == 0) PA_NOMA_pos(k, alpha, 0, L);
+        else PA_NOMA_thp(k, alpha, 0, L);
     }
+    outputfile3 << endl;
     
     outputfile1.close();
     outputfile2.close();
+    outputfile3.close();
+    outputfile4.close();
 }
